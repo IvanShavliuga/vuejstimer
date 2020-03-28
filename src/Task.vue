3c57ec961fb3aae14b2e064e447af72e6ef9001f
Ivan Shavliuga <ivanov-R0M@yandex.ru>
2020-03-28 19:30:38 +0300

    Update tasks list

--- src/Task.vue
+++ src/Task.vue
@@ -1,6 +1,6 @@
 <template>
 <tr class="task">
-       <td class="task__title"> {{task.title}}</td>
+       <td class="task__title">#{{task.id+1}}  {{task.title}}</td>
        <td class="task__message">
        {{task.message}}
        </td>
@@ -8,12 +8,23 @@
        <span class="task__time">
        {{task.hour+':'+task.minute+':'+task.second}}
        </span>
-       <span class="task__active">
+       <span :class="'task__active '+active()">
           {{active()}}
        </span>
        </td>
 </tr>
 </template>
+<style lang="scss">
+.future {
+    color:green!important;
+}
+.now {
+    color:#dd0!important;
+}
+.present {
+   color:red!important;
+}
+</style>
 <script>
 export default {
       props:{
@@ -29,12 +40,12 @@ export default {
       methods:  {        
           active(){
             let tt=this.task.hour*60*60+this.task.minute*60+this.task.second;              
-              let d=this.current.hour*60*60+this.current.minute*60+this.current.second;
+            let d=this.current.hour*60*60+this.current.minute*60+this.current.second;
             let def=tt-d;           
-              if(def > 0) return "будущее "+this.current.hour+":"+this.current.minute+":"+this.current.second;
-              else if(def===0) return "сейчас "+this.current.hour+":"+this.current.minute+":"+this.current.second;
+              if(def > 0) return "future ";//+this.current.hour+":"+this.current.minute+":"+this.current.second;
+              else if(def===0) return "now ";//+this.current.hour+":"+this.current.minute+":"+this.current.second;
             else
-                return "прошлое "+this.current.hour+":"+this.current.minute+":"+this.current.second;
+                return "present ";//+this.current.hour+":"+this.current.minute+":"+this.current.second;
            } 
        
       } 
